50a898de06b0aac8879a4515e0516b1fc6ba7b4f
Sachin patidar <sachinpatidar413@gmail.com>
2026-02-18 16:49:06 +0530

    hanging changes

--- src/components/ui/Hanging.tsx
+++ src/components/ui/Hanging.tsx
@@ -1,5 +1,5 @@
-
-"import { useEffect, useState } from "react";
+"use client";
+import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function Hanging() {
